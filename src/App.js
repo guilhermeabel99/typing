c906c8a9eb55b121afee0c6dbcfe55c2948872e7
guilhermeabel99 <gpeuvis@gmail.com>
2021-09-18 14:10:18 -0300

    add: feature validate keys

--- src/App.js
+++ src/App.js
@@ -8,12 +8,32 @@ const getWord = () => {
   const index = Math.floor(Math.random() * wordList.length)
   const word = wordList[index];
   return word.toLowerCase();
+};
+
+
+const isValidKey = (key, word) => {
+  if(!word) return false;
+  const result = word.split('').includes(key);
+  return result;
 }
 
+const Word = ({word, validKeys}) => {
+  if(!word) return null;
+  const joinedKeys = validKeys.join('');
+  const matched = word.slice(0, joinedKeys.length);
+  const remainder = word.slice(joinedKeys.length);
+
+  return(
+  <>
+  <span className="matched">{matched}</span>
+  <span className="remainder">{remainder}</span>
+  </>)
+};
 
 const App = () => {
 
     const [typedKeys, setTypedKeys] = useState([]);
+    const [validKeys, setValidKeys] = useState([]);
     const [word, setWord] = useState('');
 
     useEffect(()=>{
@@ -26,6 +46,15 @@ const App = () => {
       e.preventDefault();
       const { key } = e;
       setTypedKeys((prev) => [...prev, key].slice(MAX_TYPED_KEYS * -1));
+
+      if(isValidKey(key, word)) {
+          setValidKeys((prev)=>{
+            const isValidLength = prev.length <= word.length;
+            const isNextChar = isValidLength && word[prev.length] === key;
+            
+            return isNextChar ?  [...prev, key] : prev;
+          });
+      };
 
       console.log('key', key);
       
@@ -40,8 +69,7 @@ const App = () => {
   return (
     <div className="container" tabIndex="0" onKeyDown={handleKeyDown}>
       <div className="valid-keys">
-        <span className="matched"></span>
-        <span className="remainder">{word}</span>
+        <Word word={word} validKeys={validKeys}/>
       </div>
       <div className="typed-keys">{typedKeys ? typedKeys.join(' ') : null}</div>
       <div className="completed-words">
